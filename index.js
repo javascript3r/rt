97f77cace6ebbc3ecee468bbc59fec26a2fdba3c
javascript3r <96706937+javascript3r@users.noreply.github.com>
2021-12-27 03:28:04 +0100

    haha

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+
+
+
+
+
+let obj = ""
+
+
+function createDiv(i){
+    let div = document.createElement('div')
+    let a = document.createElement('a')
+    let img = document.createElement('img')
+    img.src = obj.data.children.at(i).data.thumbnail;
+    a.href = obj.data.children.at(i).data.url;
+    div.append(a)
+    a.append(img)
+    return div
+    }
+
